4d611bc80700626503d1df58484e7ee2b2b9dbe5
ljupce111 <ljupcemadafaka@gmail.com>
2025-02-21 20:16:29 +0100

    Delete homework4 directory

--- homework4/script.js
+++
@@ -1,8 +0,0 @@
-import{ academy } from'./academy.js'
-import { Student } from './Student.js';
-import { Subject } from './Subject.js';
-
-const newSubject = new Subject("Very Advanced JS", 5);
-console.log(newSubject);
-const studen= new Student("Jon","Doe",19,academy,"Very Advanced JS")
-console.log(studen)
